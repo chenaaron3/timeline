bf083941785db7fd3de31c8e1772ac560668611e
Aaron Chen <aaron@Aarons-Air.lan>
2025-01-13 00:47:55 -0500

    Make shadow transparent

--- src/components/Board/ShadowCard.tsx
+++ src/components/Board/ShadowCard.tsx
@@ -8,18 +8,20 @@ interface ShadowCardProps {
 export const ShadowCard: React.FC<ShadowCardProps> = ({ message, intent }) => {
     const backgroundColor = intent ? "var(--sub-main-color)" : "#4B5563"
     const outlineColor = intent ? "var(--main-color)" : "#d1d5db"
+    const opacity = intent ? 1 : .7
     return <motion.div
         className={`relative w-20 min-w-20 h-48 sm:min-w-40 sm:w-40 sm:h-60 rounded-3xl overflow-hidden bg-opacity-90 outline-dashed`}
-        initial={{ scale: .75 }}
+        initial={{ scale: .85 }}
         animate={{
             scale: 1,
             backgroundColor: backgroundColor,
             outlineColor: outlineColor,
+            opacity: opacity,
         }}
         layoutId="shadow"
     >
         <div className="flex items-center justify-center h-full">
-            <span style={{ color: outlineColor }}>{message ? message : "Place"}</span>
+            <span className='mb-5' style={{ color: outlineColor }}>{message ? message : "Place"}</span>
         </div>
     </motion.div>
 }
